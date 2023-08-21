88558823c538fe243b1290e163c623dbcd0aae05
Arman <arman-hakobyan@gmx.de>
2023-08-21 21:08:57 +0200

    new

--- src/homework_25/HomeWork_25.java
+++ src/homework_25/HomeWork_25.java
@@ -6,11 +6,6 @@ public boolean strEndsWithAnotherStr ( String str1, str2)
  */
 
 public class HomeWork_25 {
-    public static void main(String[] args) {
-        String str1 = "Hello World";
-        String str2 = "World is big";
-
-    }
 
     public boolean strEndsWithAnotherStr(String str1, String str2) {
         if (str1.endsWith(str2)) {
