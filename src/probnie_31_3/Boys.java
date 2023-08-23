64cda1461bba137c1bfa4aa82f6b5171c82d5d24
Arman <arman-hakobyan@gmx.de>
2023-08-23 16:03:27 +0200

    new

--- src/probnie_31_3/Boys.java
+++ src/probnie_31_3/Boys.java
@@ -1,7 +1,5 @@
 package probnie_31_3;
 
-import probnie_31_3.People;
-
 public class Boys extends People {
     public Boys(String name, String secondName, int age, String gender) {
         super(name, secondName, age, gender);
--- src/probnie_31_3/Girls.java
+++ src/probnie_31_3/Girls.java
@@ -1,7 +1,5 @@
 package probnie_31_3;
 
-import probnie_31_3.People;
-
 public class Girls extends People {
     public Girls(String name, String secondName, int age, String gender) {
 
